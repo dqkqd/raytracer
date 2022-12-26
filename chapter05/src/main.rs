5c873e5d25adb37c5a5afd0e9402742f8ddf203f
Duong Quoc Khanh <dqkqdlot@gmail.com>
2022-12-27 04:21:05 +0900

    Include traits Object.

--- chapter05/src/main.rs
+++ chapter05/src/main.rs
@@ -1,4 +1,4 @@
-use raytracer::{Canvas, Color, Point, Ray, Sphere};
+use raytracer::{object::ObjectWorld, Canvas, Color, Point, Ray, Sphere};
 
 const IMAGE_PPM: &str = "test.ppm";
 const IMAGE_PNG: &str = "test.png";

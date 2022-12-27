48667a1421fc50918eabf8472e1eec0e2c727db3
Duong Quoc Khanh <dqkqdlot@gmail.com>
2022-12-27 22:46:24 +0900

    Change equality test in `solve_quadratic_equation`.

--- raytracer/src/util.rs
+++ raytracer/src/util.rs
@@ -5,8 +5,8 @@ pub(super) fn equal(a: f64, b: f64) -> bool {
 }
 
 pub(crate) fn solve_quadratic_equation(a: f64, b: f64, c: f64) -> Vec<f64> {
-    if a == 0.0 {
-        if b == 0.0 {
+    if equal(a.abs(), 0.0) {
+        if equal(b.abs(), 0.0) {
             return Vec::new();
         } else {
             return vec![-c / b];
@@ -16,7 +16,7 @@ pub(crate) fn solve_quadratic_equation(a: f64, b: f64, c: f64) -> Vec<f64> {
     if discriminant > 0.0 {
         let sqrt_d = discriminant.sqrt();
         vec![(-b - sqrt_d) / (2.0 * a), (-b + sqrt_d) / (2.0 * a)]
-    } else if discriminant == 0.0 {
+    } else if equal(discriminant.abs(), 0.0) {
         vec![-b / (2.0 * a)]
     } else {
         vec![]

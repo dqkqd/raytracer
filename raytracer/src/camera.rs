4d024dbfeba2c0205037f708be6eb48bfacc2f3e
Duong Quoc Khanh <dqkqdlot@gmail.com>
2022-12-27 17:59:45 +0900

    Add `ray_for_pixel`, which convert point in canvas to world.

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -1,6 +1,6 @@
 use crate::{
     transform::{transformable, InversedTransform},
-    Transform,
+    Point, Ray, Transform,
 };
 
 pub struct Camera {
@@ -12,6 +12,8 @@ pub struct Camera {
     half_width: f64,
     half_height: f64,
 }
+
+transformable!(Camera);
 
 impl Camera {
     pub fn new(hsize: usize, vsize: usize, field_of_view: f64) -> Camera {
@@ -35,13 +37,25 @@ impl Camera {
             half_height,
         }
     }
-}
 
-transformable!(Camera);
+    pub(crate) fn ray_for_pixel(&self, x: usize, y: usize) -> Option<Ray> {
+        let xoffset = (x as f64 + 0.5) * self.pixel_size;
+        let yoffset = (y as f64 + 0.5) * self.pixel_size;
+
+        let world_x = self.half_width - xoffset;
+        let world_y = self.half_height - yoffset;
+
+        let pixel = self.inversed_transform? * Point::new(world_x, world_y, -1.0);
+        let origin = self.inversed_transform? * Point::new(0.0, 0.0, 0.0);
+        let direction = (pixel - origin).normalize();
+
+        Some(Ray::new(origin, direction))
+    }
+}
 
 #[cfg(test)]
 mod test {
-    use crate::util::assert_float_eq;
+    use crate::{transform::Transformable, util::assert_float_eq, Point, Vector};
 
     use super::*;
 
@@ -68,5 +82,38 @@ mod test {
     fn pixel_size_for_vertical_canvas() {
         let c = Camera::new(125, 200, std::f64::consts::FRAC_PI_2);
         assert_float_eq!(c.pixel_size, 0.01);
+    }
+
+    #[test]
+    fn constructing_ray_through_center_of_canvas() {
+        let c = Camera::new(201, 101, std::f64::consts::FRAC_PI_2);
+        let r = c.ray_for_pixel(100, 50).unwrap();
+        assert_eq!(r.origin(), Point::new(0.0, 0.0, 0.0));
+        assert_eq!(r.direction(), Vector::new(0.0, 0.0, -1.0));
+    }
+
+    #[test]
+    fn constructing_ray_through_a_corner_of_the_canvas() {
+        let c = Camera::new(201, 101, std::f64::consts::FRAC_PI_2);
+        let r = c.ray_for_pixel(0, 0).unwrap();
+        assert_eq!(r.origin(), Point::new(0.0, 0.0, 0.0));
+        assert_eq!(r.direction(), Vector::new(0.66519, 0.33259, -0.66851));
+    }
+
+    #[test]
+    fn constructing_ray_when_camera_is_transformed() {
+        let c = Camera::new(201, 101, std::f64::consts::FRAC_PI_2).with_transform(
+            Transform::translation(0.0, -2.0, 5.0).rotate_y(std::f64::consts::FRAC_PI_4),
+        );
+        let r = c.ray_for_pixel(100, 50).unwrap();
+        assert_eq!(r.origin(), Point::new(0.0, 2.0, -5.0));
+        assert_eq!(
+            r.direction(),
+            Vector::new(
+                std::f64::consts::FRAC_1_SQRT_2,
+                0.0,
+                -std::f64::consts::FRAC_1_SQRT_2
+            )
+        );
     }
 }

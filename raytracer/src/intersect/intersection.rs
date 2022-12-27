38c2d0001202377ae44fe94734bb45e733ce184b
Duong Quoc Khanh <dqkqdlot@gmail.com>
2022-12-27 14:56:17 +0900

    Add `ComputedIntersection`.

--- raytracer/src/intersect/intersection.rs
+++ raytracer/src/intersect/intersection.rs
@@ -1,4 +1,4 @@
-use crate::Shape;
+use crate::{object::ObjectWorld, Point, Ray, Shape, Vector};
 
 pub(crate) type IntersectionsFactor = Vec<f64>;
 
@@ -21,6 +21,30 @@ impl<'a> Intersection<'a> {
     pub(crate) fn object(&self) -> &Shape {
         self.object
     }
+
+    pub(crate) fn prepare_computations(self, ray: &Ray) -> Option<ComputedIntersection<'a>> {
+        let t = self.t;
+        let object = self.object;
+        let point = ray.position(t);
+        let eye_vector = -ray.direction();
+        let normal_vector = object.normal_at(&point)?;
+        Some(ComputedIntersection {
+            t,
+            object,
+            point,
+            eye_vector,
+            normal_vector,
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub(crate) struct ComputedIntersection<'a> {
+    object: &'a Shape,
+    t: f64,
+    point: Point,
+    eye_vector: Vector,
+    normal_vector: Vector,
 }
 
 #[cfg(test)]
@@ -34,5 +58,18 @@ mod test {
         let i = Intersection::new(3.5, &s);
         assert_eq!(i.t(), 3.5);
         assert_eq!(i.object(), &s);
+    }
+
+    #[test]
+    fn precomputing_state_of_intersection() {
+        let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::shape();
+        let i = Intersection::new(4.0, &s);
+        let comps = i.prepare_computations(&r).unwrap();
+        assert_eq!(comps.t, i.t);
+        assert_eq!(comps.object, i.object);
+        assert_eq!(comps.point, Point::new(0.0, 0.0, -1.0));
+        assert_eq!(comps.eye_vector, Vector::new(0.0, 0.0, -1.0));
+        assert_eq!(comps.normal_vector, Vector::new(0.0, 0.0, -1.0));
     }
 }

82892e0a4517365285d1132009c3dfd562b9de6f
Duong Quoc Khanh <dqkqdlot@gmail.com>
2022-12-28 18:02:14 +0900

    Add ring pattern.

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,6 +1,6 @@
 use raytracer::{
     color, shapes::ShapeMaterial, Camera, CheckerPattern, Color, GradientPattern, Plane, Point,
-    PointLight, Sphere, StripedPattern, Transform, Transformable, Vector, World,
+    PointLight, RingPattern, Sphere, StripedPattern, Transform, Transformable, Vector, World,
 };
 
 const IMAGE_PPM: &str = "test.ppm";
@@ -32,18 +32,22 @@ fn main() {
         .with_specular(0.3)
         .with_transform(Transform::scaling(0.5, 0.5, 0.5).translate(1.5, 0.5, -0.5))
         .with_pattern(
-            GradientPattern::pattern(color::BLACK, Color::from("#00ffff"))
-                .with_transform(Transform::rotation_y(std::f64::consts::FRAC_PI_2 * 1.2)),
+            RingPattern::pattern(Color::from("#ff0000"), Color::from("#0000ff"))
+                .with_transform(Transform::scaling(0.05, 1.0, 1.0).rotate_y(1.6)),
         );
 
     let left = Sphere::shape()
         .with_color(Color::new(1.0, 0.8, 0.1))
         .with_diffuse(0.7)
         .with_specular(0.3)
-        .with_transform(Transform::scaling(0.33, 0.33, 0.33).translate(-1.5, 0.33, -0.75));
+        .with_transform(Transform::scaling(0.33, 0.33, 0.33).translate(-1.5, 0.33, -0.75))
+        .with_pattern(
+            GradientPattern::pattern(color::BLACK, Color::from("#00ffff"))
+                .with_transform(Transform::rotation_y(std::f64::consts::FRAC_PI_2 * 0.8)),
+        );
 
     let light1 = PointLight::new(Point::new(-10.0, 10.0, -10.0), color::WHITE);
-    let light2 = PointLight::new(Point::new(20.0, 10.0, -10.0), color::WHITE);
+    let light2 = PointLight::new(Point::new(15.0, 10.0, -10.0), color::WHITE);
     let world = World::new(vec![light1, light2], vec![floor, middle, right, left]);
 
     let mut camera = Camera::new(800, 500, std::f64::consts::FRAC_PI_3);

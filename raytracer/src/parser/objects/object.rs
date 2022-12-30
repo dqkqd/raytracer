59943b6605267e6e3a9dd22306bdc947aebf42bc
Duong Quoc Khanh <dqkqdlot@gmail.com>
2022-12-31 01:57:30 +0900

    Move parse test to `yaml` module.

--- raytracer/src/parser/objects/object.rs
+++ raytracer/src/parser/objects/object.rs
@@ -17,32 +17,10 @@ impl Object {
             _ => unimplemented!(),
         }
     }
-    fn as_camera(&self) -> Option<&Camera> {
+    pub fn as_camera(&self) -> Option<&Camera> {
         match self {
             Object::Camera(camera) => Some(camera),
             _ => None,
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::parser::yaml;
-
-    use super::*;
-
-    #[test]
-    fn parse_object_from_attribute() {
-        let yaml = "
-- add : camera
-  width: 10
-  height: 20
-  field-of-view: 1.25
-  from: [ 1, 2, 3 ]
-  to: [ 4, 5, 6 ]
-  up: [ 7, 8, 9 ]
-";
-        let objects = yaml::from_str(yaml).unwrap();
-        assert!(objects[0].as_camera().is_some());
     }
 }
--- raytracer/src/parser/yaml.rs
+++ raytracer/src/parser/yaml.rs
@@ -381,4 +381,19 @@ transform:
         ";
         assert_value(&cube.value, expected)
     }
+
+    #[test]
+    fn parse_camera_from_str() {
+        let yaml = "
+- add : camera
+  width: 10
+  height: 20
+  field-of-view: 1.25
+  from: [ 1, 2, 3 ]
+  to: [ 4, 5, 6 ]
+  up: [ 7, 8, 9 ]
+";
+        let objects = from_str(yaml).unwrap();
+        assert!(objects[0].as_camera().is_some());
+    }
 }
